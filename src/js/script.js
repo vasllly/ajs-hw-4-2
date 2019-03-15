b61d3466bd993d8f912c8087b22710014c43d372
Ganja Vasiliy <vasllly219@gmail.com>
2019-03-15 17:34:46 +0300

    DONE

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,11 @@
-export default function indication(character) {
-  let status;
-  if (character.health > 50) {
-    status = 'healthy';
-  } else if (character.health >= 15) {
-    status = 'wounded';
-  } else {
-    status = 'critical';
-  }
-  return status;
+export default function sortHeroes(heroes) {
+  return heroes.sort((a, b) => {
+    if (a.health > b.health) {
+      return -1;
+    }
+    if (a.health < b.health) {
+      return 1;
+    }
+    return 0;
+  });
 }
--- test/test.js
+++ test/test.js
@@ -1,36 +1,18 @@
-import indication from '../src/js/script';
+import sortHeroes from '../src/js/script';
 
-test('should calculate total for purchases', () => {
-  const input = { name: 'Маг', health: 90 };
-  const expected = 'healthy';
-  const received = indication(input);
-  expect(received).toBe(expected);
-});
-
-test('should calculate total for purchases', () => {
-  const input = { name: 'Маг', health: 50 };
-  const expected = 'wounded';
-  const received = indication(input);
-  expect(received).toBe(expected);
-});
-
-test('should calculate total for purchases', () => {
-  const input = { name: 'Маг', health: 30 };
-  const expected = 'wounded';
-  const received = indication(input);
-  expect(received).toBe(expected);
-});
-
-test('should calculate total for purchases', () => {
-  const input = { name: 'Маг', health: 15 };
-  const expected = 'wounded';
-  const received = indication(input);
-  expect(received).toBe(expected);
-});
-
-test('should calculate total for purchases', () => {
-  const input = { name: 'Маг', health: 10 };
-  const expected = 'critical';
-  const received = indication(input);
-  expect(received).toBe(expected);
+test('sorted heroes', () => {
+  const input = [
+    { name: 'врач', health: 80 },
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+  ];
+  const expected = [
+    { name: 'маг', health: 100 },
+    { name: 'врач', health: 80 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+  ];
+  const received = sortHeroes(input);
+  expect(received).toEqual(expected);
 });
